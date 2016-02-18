3527a20dcd43e5a5c5c67cfd2e1d14185e4b9f50
Mukolajko <borusyuk_kolya@ukr.net>
2016-02-19 01:41:44 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,106 @@
+(function(){
+    //angular logic
+    var app = angular.module("store", []);
+
+    //store controller
+    app.controller("StoreController", function(){
+        this.products = gems;
+    });
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller("ReviewController", function(){
+        this.review = {};
+
+        this.addReview = function (product) {
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+
+        this.show = function()  {
+            return !(Object.keys(this.review).length === 0);
+        }
+    });
+
+
+
+
+
+
+
+    var gems = [
+        {
+            name: "Good gem",
+            price: 3.3,
+            description: "Some good gem that can be found in hole",
+            images: [
+                {
+                    full: "public/img/gem.png",
+                    thumb: "public/img/gem.png"
+                },
+                {
+                    full: "public/img/gem.png",
+                    thumb: "public/img/gem.png"
+                }
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "Nice one",
+                    author: "Test@gmail.com",
+                    createdOn: 1397490980837
+                },
+                {
+                    stars: 2,
+                    body: "Boooo",
+                    author: "Booo@gmail.com",
+                    createdOn: 1397490980837
+                }
+            ],
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: "Excellent gem",
+            price: 12.1,
+            description: "Excellent green gem",
+            images: [
+                {
+                    full: "public/img/gem.png",
+                    thumb: "public/img/gem.png"
+                },
+                {
+                    full: "public/img/gem.png",
+                    thumb: "public/img/gem.png"
+                }
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this awesome stuff",
+                    author: "Test@gmail.com",
+                    createdOn: 1397490980837
+                },
+                {
+                    stars: 2,
+                    body: "Boooo",
+                    author: "Booo@gmail.com",
+                    createdOn: 1397490980837
+                }
+            ],
+            canPurchase: false,
+            soldOut: true
+        }
+    ]
+})();
