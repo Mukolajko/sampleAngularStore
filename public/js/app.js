63c5c15f78afdc178265a8cf58f47cfc76e807b7
Mukolajko <borusyuk_kolya@ukr.net>
2016-02-21 13:27:15 +0200

    add directive

--- public/js/app.js
+++ public/js/app.js
@@ -33,10 +33,26 @@
         }
     });
 
+    app.directive('productTitle', function(){
+       return {
+           restrict: "A",
+           templateUrl: "product-title.html"
+       };
+    });
 
-
-
-
+    app.directive('productGallery', function(){
+        return {
+            restrict: "E",
+            templateUrl: "product-gallery.html",
+            controller: function(){
+                this.current = 0;
+                this.setCurrent = function(imageNumber){
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: "gallery"
+        };
+    });
 
 
     var gems = [
